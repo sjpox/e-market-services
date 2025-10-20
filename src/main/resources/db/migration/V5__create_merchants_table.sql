163672ff9baa0dee02910d029fe6b9c84e29f715
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-10-20 22:47:18 +0800

    feat: add create merchants sql script

---
+++ src/main/resources/db/migration/V5__create_merchants_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE merchants (
+  merchant_id     VARCHAR(50) PRIMARY KEY,
+  merchant_name   VARCHAR(255) NOT NULL,
+  email           VARCHAR(255) UNIQUE,
+  contact_number  VARCHAR(50),
+  address         TEXT,
+  created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
