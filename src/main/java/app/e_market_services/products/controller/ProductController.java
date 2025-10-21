57a23ae430c13e5bd33ac6aa3b1b8c2b4e8cfc60
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-10-21 21:54:46 +0800

    feat: refactor products module

--- src/main/java/app/e_market_services/products/controller/ProductController.java
+++ src/main/java/app/e_market_services/products/controller/ProductController.java
@@ -4,26 +4,29 @@ import java.util.List;
 
 import app.e_market_services.common.constant.HttpStatusDesc;
 import app.e_market_services.common.response.ApiResponse;
-import app.e_market_services.products.dto.ProductResponseDto;
+import app.e_market_services.products.dto.ProductsResponse;
 import app.e_market_services.products.service.ProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 public class ProductController {
     private final ProductService productService;
+
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @GetMapping
-    public ResponseEntity<ApiResponse<List<ProductResponseDto>>> findAllProducts() {
+    public ResponseEntity<ApiResponse<List<ProductsResponse>>> findAllProducts() throws JsonProcessingException {
         return ResponseEntity.ok()
                 .body(ApiResponse
-                        .<List<ProductResponseDto>>builder()
+                        .<List<ProductsResponse>>builder()
                         .status(HttpStatusDesc.SUCCESS)
                         .result(productService.findAll())
                         .build());
--- src/main/java/app/e_market_services/products/model/Products.java
+++ src/main/java/app/e_market_services/products/model/Products.java
@@ -2,11 +2,11 @@ package app.e_market_services.products.model;
 
 import app.e_market_services.categories.model.Categories;
 import app.e_market_services.mechants.model.Merchants;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,35 +22,38 @@ import java.util.UUID;
 @Table(name = "products")
 public class Products {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID product_id;
-    private String product_name;
+    @UuidGenerator
+    @Column(name = "product_id")
+    private String productId;
+    private String productName;
     private String description;
     private BigDecimal price;
-    private int stock_quantity;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
+    private int stockQuantity;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "product_categories",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private Set<Categories> categories = new HashSet<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "merchant_id")
     private Merchants merchants;
 
     @PrePersist
     public void prePersist() {
-        created_at = LocalDateTime.now();
-        updated_at = LocalDateTime.now();
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
     }
 
     @PreUpdate
     public void preUpdate() {
-        updated_at = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
     }
 }
