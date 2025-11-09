df873e2e018d52b1a9cdb2809b5068ef139c421a
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-11-09 19:12:48 +0800

    feat: add corsconfig

--- src/main/java/app/e_market_services/auth/config/SecurityConfig.java
+++ src/main/java/app/e_market_services/auth/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import app.e_market_services.auth.validation.JwtAuthFilter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -22,16 +27,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        log.info("Configuring security filter chain");
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/users/login")
-                        .permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
-                        .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // ✅ modern style
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/**").permitAll()
+            )
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/v1/users/login")
+//                        .permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/v1/jwt/**").permitAll()
+//                        .anyRequest().authenticated())
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5175"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
