975e0d0be5332f271585e40b338930b22cd2a598
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-11-20 23:27:09 +0800

    feat: initial implementation of municipality module

---
+++ src/main/java/app/e_market_services/municipality/controller/MunicipalityController.java
@@ -0,0 +1,10 @@
+package app.e_market_services.municipality.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/municipality")
+public class MunicipalityController {
+
+}
