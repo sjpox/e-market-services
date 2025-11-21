f8e3bc39dbefbf3910bd956b8a1a683db38f9be3
johnpaulsale <iamjohnpaulsale12@gmail.com>
2025-11-21 23:33:07 +0800

    feat: update municipality model

--- src/main/java/app/e_market_services/municipality/model/Municipality.java
+++ src/main/java/app/e_market_services/municipality/model/Municipality.java
@@ -1,15 +1,20 @@
 package app.e_market_services.municipality.model;
 
 import app.e_market_services.base.Model.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Builder;
+import org.hibernate.annotations.UuidGenerator;
 
 @Builder
 @Entity
 @Table(name = "municipality")
 public class Municipality extends BaseEntity {
-
-
+    @Id
+    @UuidGenerator
+    @Column(name = "municipality_id")
     private String municipalityId;
+    private String municipalityName;
 }
